51849986223f355721632a701f99f6cd1f358d7a
cristian <cristian982940@gmail.com>
2019-05-13 11:41:27 -0500

    create algoritmo matematico for calculo del cotizador of segurity

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -2,10 +2,34 @@ import React, {Component} from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
 
+import {obtenerDiferenciaAnio, calcularMarca, obtenerPlan} from '../helper';
+
 class App extends Component {
 
   cotizarSeguro = datos =>{ 
-    console.log(datos)
+    const {marca, plan, year } = datos;
+
+    // Agregar una base de 2000,
+    let resultado = 2000;
+
+    // Obtener la diferencia de años 
+    const diferencia = obtenerDiferenciaAnio(year)
+     
+    // por cada año restar el 3%
+    resultado -= ((diferencia *3) * resultado) /100; 
+
+    //Dependiendo la marca seleccionada aumentara un porcentaje respectivo
+    // *Americano 15%
+    // *Asiatio 5%
+    // *Europeo 30%
+
+    resultado = calcularMarca(marca) * resultado;
+
+    //El plan del auto, el basico incrementa el valor 20% y cobertura completa 50%
+    let incrementoPlan = obtenerPlan(plan);
+
+    //dependiendo del plan incrementar
+    resultado = parseFloat(incrementoPlan * resultado).toFixed(2);
   }
 
   render(){
